6f39d9773494784be46489317ee5fd4eafe97783
Francisco Junior <franciscojn49@gmail.com>
2020-06-22 20:57:33 -0300

    feat: configuration debug.

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,8 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'okkk' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Servidor Executando! ... http://localhost:3333');
