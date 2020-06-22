2cef102a7dc88c4992459f753563f7b5c3482e7f
Francisco Junior <franciscojn49@gmail.com>
2020-06-22 17:56:10 -0300

    feat: eslint, editorconfig and prettier configuration.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express, { request, response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'okkk' });
-})
+  return response.json({ message: 'okkk' });
+});
 
-app.listen((3333), () => {
-    console.log(
-        'Servidor Executando! ... http://localhost:3333'
-    );
-})
+app.listen(3333, () => {
+  console.log('Servidor Executando! ... http://localhost:3333');
+});
