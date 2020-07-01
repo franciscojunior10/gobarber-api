faef9f419153a0f6a70544881a85ded3316a7021
Francisco Junior <franciscojn49@gmail.com>
2020-07-01 11:33:26 -0300

    feat: created model and migration user.

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
@@ -10,6 +10,12 @@ class Appointment {
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
