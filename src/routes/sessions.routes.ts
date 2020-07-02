db35f28e106220822035218ad9fc6b80e852b209
Francisco Junior <franciscojn49@gmail.com>
2020-07-02 12:22:00 -0300

    feat: validation user credential in sessions.

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request: Request, response: Response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUserService = new AuthenticateUserService();
+
+    const { user } = await authenticateUserService.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
