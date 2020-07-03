865cc93bb120380376f4585807367d468adb8183
Francisco Junior <franciscojn49@gmail.com>
2020-07-03 11:46:32 -0300

    feat: created class errors.

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -19,7 +19,7 @@ sessionsRouter.post('/', async (request: Request, response: Response) => {
 
     return response.json({ user, token });
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(err.statusCode).json({ error: err.message });
   }
 });
 
--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import fs from 'fs';
 
 import uploadConfig from '../config/upload';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +21,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
