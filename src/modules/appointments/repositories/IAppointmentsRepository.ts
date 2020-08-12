29bb1c66b3819280b0a454537ab25b7e87df8714
Francisco Junior <franciscojn49@gmail.com>
2020-08-12 14:21:22 -0300

    refactor: addyng Liskov Substitution Principle.

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,7 @@
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IAppointmentsRepository {
+  findByDate(date: Date): Promise<Appointment | undefined>;
+}
+
+export default IAppointmentsRepository;
