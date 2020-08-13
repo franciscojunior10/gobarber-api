a1a35c7fe0768a3db69df656f5dd02b811baa546
Francisco Junior <franciscojn49@gmail.com>
2020-08-13 17:40:12 -0300

    refactor: rewriting repository.

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
 
