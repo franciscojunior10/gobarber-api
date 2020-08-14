89c8a0c01adea2de47aa7eee65a90683d6548830
Francisco Junior <franciscojn49@gmail.com>
2020-08-14 14:52:55 -0300

    refactor: added Dependency Inversion Principle.

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,14 +1,14 @@
 /* eslint-disable camelcase */
 import { Router, Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -17,7 +17,7 @@ appointmentsRouter.post('/', async (request: Request, response: Response) => {
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(appointmentRepository);
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
@@ -27,11 +27,10 @@ appointmentsRouter.post('/', async (request: Request, response: Response) => {
   return response.status(200).json(appointment);
 });
 
-appointmentsRouter.get('/', async (request: Request, response: Response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request: Request, response: Response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
-export default appointmentsRouter;
+// export default appointmentsRouter;
