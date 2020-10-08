c990c9d426de25cd5afd448cc9bc415285708f31
Francisco Junior <franciscojn49@gmail.com>
2020-10-07 23:27:15 -0300

    feat: added test the authenticate.

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,48 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'Francisco Júnior',
+      email: 'franciscojn49@gmail.com',
+      password: '121212',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'Francisco Júnior',
+      email: 'franciscojn49@gmail.com',
+      password: '121212',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Francisco Júnior',
+        email: 'franciscojn49@gmail.com',
+        password: '121212',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
